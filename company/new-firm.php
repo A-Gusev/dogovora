5e0f9ba029056e9fb18bc7df742bb5ed7d2da15f
Alexey Gusev <gusev_as@gym1534.ru>
2014-10-25 13:22:55 +0400

    v.0.3.2

--- company/new-firm.php
+++ company/new-firm.php
@@ -328,7 +328,7 @@
 	/* закрываем подключение */
 	mysqli_close($link);
 ?>
-	<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+	<br /><br /><br /><nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
 	  <div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">
