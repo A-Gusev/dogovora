6bd98cb253f383d1d20f8a41ca3c5c2e6d310463
Alexey <as@Alexeys-MacBook-Pro.local>
2015-06-29 14:25:07 +0300

    delete_old

--- exit.php
+++
@@ -1,6 +0,0 @@
-<?php
-        # Обнуляем куки
-        setcookie('u-id', '', time()+60*60*24*30);
-        setcookie('hash', '', time()+60*60*24*30);
-        header("Location: user/login.php"); exit();
-?>
