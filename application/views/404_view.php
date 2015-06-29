bbbf92e082f5ec495a364ed58fa7b4cf424281a1
Alexey <as@Alexeys-MacBook-Pro.local>
2015-06-30 00:09:30 +0300

    404

--- application/views/404_view.php
+++ application/views/404_view.php
@@ -1,4 +1,7 @@
-<h1>404</h1>
-<p>
-<?php // echo '<img src="/images/404.png"'; ?>
-</p>
+<div class="text-center">
+            <div class="page-header">
+                <h1>404 Error pages</h1>
+            </div>
+                <h2>Страница не найдена</h2>
+                <img src="/images/404.png" width="216" height="411" alt="404">
+</div>
